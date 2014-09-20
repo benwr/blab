c309dc0129d18137ac0528676b0809d0594574df
alex <ahc24@vt.edu>
2014-09-20 08:31:20 -0400

    added communication stuff

--- pic_master/src/src/communcation.c
+++ pic_master/src/src/communcation.c
@@ -1,7 +1,22 @@
 #include "communication.h"
 
+static uart_packet_type uart_current_packet;
+
+static unsigned char uart_count;
+
+void send_uart_message( unsigned char * message_ptr , unsigned char uart_device_id )
+{
+    
 
 
 
 
 
+}
+
+void send_uart_header( unsigned char uart_device_id )
+{
+    unsigned char send_byte = ( uart_device_id << 3 ) | ( uart_count );
+
+
+}
--- pic_master/src/src/communication.h
+++ pic_master/src/src/communication.h
@@ -8,18 +8,28 @@
 #ifndef COMMUNICATION_H
 #define	COMMUNICATION_H
 
-#define PACKET_LENGTH 4
+#include "my_uart.h"
 
-struct packet_type
+#define MESSAGE_LENGTH 4
+#define UART_HEADER_ID_LENGTH 5
+#define UART_HEADER_COUNT_LENGTH 3
+
+typedef struct 
 {
     unsigned char header;
-    unsigned char data[ PACKET_LENGTH - 1 ];
+    unsigned char data[ MESSAGE_LENGTH - 1 ];
+    unsigned char bytes_sent;
+    unsigned char one_bits_sent;
     unsigned char checksum_error;
     unsigned char count_error;
-};
+    unsigned char byte_error;
+}uart_packet_type;
 
 
 
+void send_uart_message( unsigned char * message_ptr , unsigned char uart_device_id);
+
+void send_uart_header( unsigned char uart_device_id );
 
 
 #endif	/* COMMUNICATION_H */
