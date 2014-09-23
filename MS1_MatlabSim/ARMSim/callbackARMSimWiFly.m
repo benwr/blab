5cab0fe266596f30c77d4adc35fcf539d785eff2
hadertjou <dacrymn@gmail.com>
2014-09-23 10:42:23 -0400

    First Simulation Commit

---
+++ MS1_MatlabSim/ARMSim/callbackARMSimWiFly.m
@@ -0,0 +1,63 @@
+function callbackARMSimWiFly( obj, event, h )
+% Callback for WiFly serial object on ARMSim side
+% One would not actual do the communication in this way (i.e., via
+% string with the terminator 'o'.). This is just to show how to
+% Set up the callbacks and to communicate between the two programs.
+
+% Keep around a persistent array that we add data to as we receive it.
+% We also keep around the current plot axis data.
+persistent dataArray;
+persistent figureAxis;
+
+% We are in the callback because we received some data. We calculate
+% how much and then read it into a data array.
+bytesAvailable = obj.BytesAvailable;
+%[data count msg] = fread(obj, bytesAvailable, 'char');
+[data count msg] = fread(obj, bytesAvailable);
+%exp
+%value = fread(obj, bytesAvailable, 'byte');
+
+valuesReceived = obj.ValuesReceived;
+%data.length = length(data) - 1;
+%fprintf('callbackARMSimWiFly: Received data %d, %d bytes. Total of %d bytes read.\n',...
+%    data, bytesAvailable, valuesReceived);
+
+fprintf('Received data from sensor %d.\n Received data from sensor %d.\n Received data from sensor %d.\n', data, bytesAvailable, valuesReceived);
+
+
+% We convert the string back into a value. This is, of course, completely
+% ridiculous, and your code will not do this.
+%str = sprintf('%s',data(1:(length(data)-1)));
+%fprintf('callbackARMSimWiFly: Modified data = %s\n',str);
+%value = str2double(str);
+%fprintf('callbackARMSimWiFly: Received the value %f\n',value);
+
+% We use the value just received and include it in our dataArray. We
+% then update our plot of data received from the SensorSim.
+figure(h);
+if isempty(dataArray)
+    dataArray = [data];
+    figureAxis = axis();
+    figureAxis(1) = 0;
+    figureAxis(2) = 20;
+    figureAxis(3) = 0;
+    figureAxis(4) = 10;
+    plot(1,dataArray,'+');
+    axis(figureAxis);
+    title('Data received from SensorSim');
+    xlabel('Data index');
+    ylabel('Distance Value (cm)');
+    % We hold the plot so that we can just add data values one at a time
+    % without the figure flashing on and off.
+    hold on;
+else
+    dataArray = [dataArray data];
+    if(length(dataArray)>figureAxis(2))
+        figureAxis(2) = figureAxis(2) + 10;
+        axis(figureAxis);
+    end
+    plot(length(dataArray),data,'+');
+end
+
+end
+
